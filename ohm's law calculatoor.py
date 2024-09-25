3483a31cf88cd4f9588a9705e8f844101e93cee5
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-09-25 11:28:03 +0800

    solution for voltage

--- ohm's law calculatoor.py
+++ ohm's law calculatoor.py
@@ -1,3 +1,12 @@
 #will ask the user for calculation Voltage, Current or Resistance
+while True:
+    ohms_law = input("Type '1' if you want to solve for Voltage"
+                    "\nType '2' if you want to solve for Current"
+                    "\nType '3' if you want to solve for Resistance: ")
 #will ask the user to input with respect to user choice
+    if ohms_law == '1':
+        current = float(input("Enter current: "))
+        resistance = float(input("Enter resistance: "))
+        voltage = current * resistance
+        print("The Voltage is {voltage} Volts".format(voltage=voltage))
 #will ask the user to try again
